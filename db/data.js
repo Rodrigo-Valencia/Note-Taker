66f498e47cb05c581b9c7af3aefaee91aa512c41
Rodrigo <rodvalenciaa@gmail.com>
2022-04-15 20:53:47 -0700

    debugging

--- db/data.js
+++ db/data.js
@@ -1,2 +1,47 @@
 const fs = require('fs');
+const util = require('util');
 
+const writeNote = util.promisify(fs.writeFile);
+const readNote = util.promisify(fs.readFile);
+
+class Save {
+    read() {
+        return readNote('db/db.json');
+    }
+    write(note) {
+        return writeNote('db/db.json', JSON.stringify(note));
+    }
+
+    returnNotes() {
+        return this.read().then(notes => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+
+    newNote(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error('Can not enter blank note!');
+        }
+        const addNewNote = { title, text };
+
+        return this.returnNotes()
+            .then(notes => [...notes, addNewNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => addNewNote);
+    }
+
+    trashNote(id) {
+        return this.returnNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filteredNotes => this.write(filteredNotes));
+    }
+}
+
+module.exports = new Save();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const data = require('../db/data');
 
+router.get('/notes', function (req, res) {
+    data.returnNotes().then(notes => res.json(notes)).catch(err => res.status(500).json(err));
+});
 
+router.post('/notes', (req, res) => {
+    data.newNote(req.body).then((note) => res.json(note)).catch(err => res.status(500).json(err));
+});
+
+router.delete('/notes/:id', function (req, res) {
+    data.trashNote(req.params.id).then(() => res.json({ ok: true })).catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
